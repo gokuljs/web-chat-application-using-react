7e566954932e404cc2f585552a480607babc535d
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-16 13:12:37 +0530

    chat app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 import React from 'react';
-import {chatEngine} from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 import './app.css'
+
+const App=() =>{
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="827382dd-cbe5-4445-94a1-327f7c387452"
+        userName="diablo"
+        userSecret="123456"
+        />
+    );
+}
+
+export default App;
