cd5d79d4c658f77c76df2686e21d83bb1092d00a
gokuljs <gokuljs029@gmail.com>
2021-06-10 09:06:23 +0530

    working with their messages

--- src/components/TheirMessage/TheirMessage.component.jsx
+++ src/components/TheirMessage/TheirMessage.component.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 // handle the message other prople sent 
 
 const TheirMessage=({lastMessage,message})=>{
+    // when working with someone elses ,message we want to know is it the first message by that user 
+    const isFirstMessageByUser=!lastMessage ||lastMessage.sender.username !== message.sender.username
     return(
-        <div>
-           Their message
+        <div className="message-row">
+           {isFirstMessageByUser &&(
+               <div
+               className="message-avatar"
+               style={{backgroundImage:`url(${message?.sender?.avatar})`}}
+                />
+
+           )}
+           {
+               // now checking out message is an attachment or an image
+            (message?.attachmeans?.lenght>0)
+         ?(
+            <img src={message.attachment[0].file}
+            alt="message-attachment"
+            className="message-image"
+            styles={{float:'right'}}
+             />
+            ):( 
+                <div
+               className="message-avatar"
+               style={{backgroundImage:`url(${message?.sender?.avatar})`}}
+            />
+             )
+    
+           }
         </div>
     )
 }
